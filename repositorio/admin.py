5c96f887ef4985dabf326e3651e0924d93d8e7d4
Rafael <jimenezrafael@gmail.com>
2025-10-30 12:26:20 +0100

    seleccionar grupos en perfiles

--- repositorio/admin.py
+++ repositorio/admin.py
@@ -5,19 +5,20 @@ Acá se ingresan los modelos al panel que administra el superusuario.
 from django.contrib import admin
 from .models import Perfil, Documento, Grupo
 
-@admin.register(Perfil)
-class PerfilAdmin(admin.ModelAdmin):
-    list_display = ('user', 'tipo', 'grupo')
-    list_filter = ('tipo', 'grupo')
-    search_fields = ('user__username',)
-
 @admin.register(Grupo)
 class GrupoAdmin(admin.ModelAdmin):
     list_display = ('nombre',)
     search_fields = ('nombre',)
 
+@admin.register(Perfil)
+class PerfilAdmin(admin.ModelAdmin):
+    list_display = ('user', 'tipo', 'grupo')
+    search_fields = ('user__username', 'tipo')
+    list_filter = ('tipo', 'grupo')
+    autocomplete_fields = ('grupo',)  # 🔹 Permite elegir grupos existentes
+
 @admin.register(Documento)
 class DocumentoAdmin(admin.ModelAdmin):
     list_display = ('titulo', 'docente', 'grupo', 'creado')
-    list_filter = ('grupo', 'docente')
-    search_fields = ('titulo',)
+    search_fields = ('titulo', 'docente__username')
+    list_filter = ('grupo', 'creado')
