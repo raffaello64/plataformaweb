cc3a71c8a4d2aecbe795d1594039024ad6f81f2d
Rafael <jimenezrafael@gmail.com>
2025-10-30 12:00:32 +0100

    actualización asignación de grupos

--- repositorio/admin.py
+++ repositorio/admin.py
@@ -2,22 +2,22 @@
 Acá se ingresan los modelos al panel que administra el superusuario.
 """
 
-
 from django.contrib import admin
 from .models import Perfil, Documento, Grupo
 
 @admin.register(Perfil)
 class PerfilAdmin(admin.ModelAdmin):
     list_display = ('user', 'tipo', 'grupo')
-    search_fields = ('user__username', 'tipo')
     list_filter = ('tipo', 'grupo')
-
-@admin.register(Documento)
-class DocumentoAdmin(admin.ModelAdmin):
-    list_display = ('titulo', 'docente', 'grupo', 'creado')
-    search_fields = ('titulo', 'docente__username')
-    list_filter = ('grupo', 'creado')
+    search_fields = ('user__username',)
 
 @admin.register(Grupo)
 class GrupoAdmin(admin.ModelAdmin):
     list_display = ('nombre',)
+    search_fields = ('nombre',)
+
+@admin.register(Documento)
+class DocumentoAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'docente', 'grupo', 'creado')
+    list_filter = ('grupo', 'docente')
+    search_fields = ('titulo',)
--- repositorio/models.py
+++ repositorio/models.py
@@ -8,6 +8,7 @@ y las correlaciones adentro del programa
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class Grupo(models.Model):
     nombre = models.CharField(max_length=100, unique=True)
 
